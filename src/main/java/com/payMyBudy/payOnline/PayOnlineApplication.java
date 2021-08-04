23d3b382fde0bcc2601bcb7feef87b1a0fedaa58
Thibisault <72867272+Thibisault@users.noreply.github.com>
2021-08-04 11:13:50 +0200

    premier commit

---
+++ src/main/java/com/payMyBudy/payOnline/PayOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.payMyBudy.payOnline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayOnlineApplication.class, args);
+	}
+
+}
